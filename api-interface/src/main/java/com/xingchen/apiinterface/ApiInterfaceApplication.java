70770dafb4a5a7b8f6cb5b12cf0837a6f13d73da
linxun1209 <3056726345@qq.com>
2024-02-29 22:02:03 +0800

    添加最活跃用户的接口

--- api-interface/src/main/java/com/xingchen/apiinterface/ApiInterfaceApplication.java
+++ api-interface/src/main/java/com/xingchen/apiinterface/ApiInterfaceApplication.java
@@ -2,12 +2,12 @@ package com.xingchen.apiinterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author xing'chen
  */
-@SpringBootApplication
-
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ApiInterfaceApplication {
 
     public static void main(String[] args) {
--- openplatform/src/main/java/com/xingchen/openplatform/controller/AnalysisController.java
+++ openplatform/src/main/java/com/xingchen/openplatform/controller/AnalysisController.java
@@ -3,6 +3,7 @@ package com.xingchen.openplatform.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 
 import com.xingchen.common.model.entity.InterfaceInfo;
+import com.xingchen.common.model.entity.User;
 import com.xingchen.common.model.entity.UserInterfaceInfo;
 import com.xingchen.openplatform.annotation.AuthCheck;
 import com.xingchen.openplatform.common.BaseResponse;
@@ -11,7 +12,9 @@ import com.xingchen.openplatform.common.ResultUtils;
 import com.xingchen.openplatform.exception.BusinessException;
 import com.xingchen.openplatform.mapper.UserInterfaceInfoMapper;
 import com.xingchen.openplatform.model.vo.InterfaceInfoVO;
+import com.xingchen.openplatform.model.vo.UserInterfaceVO;
 import com.xingchen.openplatform.service.InterfaceInfoService;
+import com.xingchen.openplatform.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.CollectionUtils;
@@ -38,6 +41,13 @@ public class AnalysisController {
     @Resource
     private InterfaceInfoService interfaceInfoService;
 
+    @Resource
+    private UserService userService;
+
+    /**
+     * 接口被调用次数TOP5
+     * @return
+     */
     @GetMapping("/top/interface/invoke")
     @AuthCheck(mustRole = "admin")
     public BaseResponse<List<InterfaceInfoVO>> listTopInvokeInterfaceInfo() {
@@ -56,6 +66,33 @@ public class AnalysisController {
             int totalNum = interfaceInfoIdObjMap.get(interfaceInfo.getId()).get(0).getTotalNum();
             interfaceInfoVO.setTotalNum(totalNum);
             return interfaceInfoVO;
+        }).collect(Collectors.toList());
+        return ResultUtils.success(interfaceInfoVOList);
+    }
+    /**
+     * 最活跃的用户TOP5
+     * @return
+     */
+    @GetMapping("/top/interface/user")
+    @AuthCheck(mustRole = "admin")
+    public BaseResponse<List<UserInterfaceVO>> listTopUserInvokeInterfaceInfo() {
+        List<UserInterfaceInfo> userInterfaceInfoList = userInterfaceInfoMapper.listTopUserInvokeInterfaceInfo(5);
+        Map<Long, List<UserInterfaceInfo>> userIdObjMap = userInterfaceInfoList.stream()
+                .collect(Collectors.groupingBy(UserInterfaceInfo::getUserId));
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.in("id", userIdObjMap.keySet());
+        List<User> list = userService.list(queryWrapper);
+        if (CollectionUtils.isEmpty(list)) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR);
+        }
+        //list为userId的信息
+        //todo 只需返回用户信息即可
+        List<UserInterfaceVO> interfaceInfoVOList = list.stream().map(user -> {
+            UserInterfaceVO userVO = new UserInterfaceVO();
+            BeanUtils.copyProperties(user, userVO);
+            int totalNum = userIdObjMap.get(user.getId()).get(0).getTotalNum();
+            userVO.setTotalNum(totalNum);
+            return userVO;
         }).collect(Collectors.toList());
         return ResultUtils.success(interfaceInfoVOList);
     }
